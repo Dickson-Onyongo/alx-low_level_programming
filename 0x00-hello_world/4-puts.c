13f10712e44adcc8954c339eba262f3e57ceee6e
Dickson-Onyongo <dicksononyong@gmail.com>
2023-12-17 12:11:22 -0800

    new update on puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main -entry point
+ * main - entry point
  *
- * return: return 0.
+ * return: always return 0
  */
 int main(void)
 {
-		puts("\"Programming islike building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
