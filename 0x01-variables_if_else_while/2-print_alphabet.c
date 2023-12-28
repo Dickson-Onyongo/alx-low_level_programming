6dc02fc815117ba0aba64ca893d6230e731774b1
Dickson-Onyongo <dicksononyong@gmail.com>
2023-12-28 03:38:23 -0800

    alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-/*
-*main - Entry point
+/**
+* main - Entry point
 *
-*Return: Always 0 (success)
+* Return: Always 0 (success)
 */
-int main()
+int main(void)
 {
-	char ch();
+	char ch(void);
 
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
