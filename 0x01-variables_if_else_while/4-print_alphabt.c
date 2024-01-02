7f261f046f0aad55c06ad386ab8b36e683692f86
Dickson-Onyongo <dicksononyong@gmail.com>
2024-01-02 05:56:24 -0800

    print alphabet in lowewrcase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always return 0 upon success.
+**/
+int main(void)
+{
+
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		if (ch == 'q' || == 'e')
+		{
+		continue;
+
+		}
+	putchar(ch);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
