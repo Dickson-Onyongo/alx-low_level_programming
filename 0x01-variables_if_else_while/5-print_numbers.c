9f8874ff168fd5af871305299c7fc91418394782
Dickson-Onyongo <dicksononyong@gmail.com>
2024-01-02 06:04:31 -0800

    base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: returns 0 upon success.
+**/
+
+int main(void)
+{
+	int a ;
+
+	while(a = 0, a <= 10)
+	{
+		a++;
+
+	printf("%d\n", a);
+
+	}
+
+	return(0);
+
+}
