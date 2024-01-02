ad13adb57028525d8a3ab2d5703a3af3c0fcf3d7
Dickson-Onyongo <dicksononyong@gmail.com>
2024-01-02 06:47:37 -0800

    base 10 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - Entry point.
+*
+* Return: returns 0 upon success
+*/
+int main(void)
+{
+	char ch;
+	ch = 0;
+
+	while (ch < 10)
+	{
+	putchar(ch);
+	
+	ch++;
+
+	}
+
+	putchar("\n");
+
+	return (0);
+
+}
