f75492a8c5bd43dcafae42710cbc67240e6776f1
Dickson-Onyongo <dicksononyong@gmail.com>
2024-01-02 07:39:17 -0800

    base 16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: returns 0 upon success.
+**/
+
+int main()
+{
+	int x;
+
+	x = 0;
+
+	while (x < 16)
+	{
+
+	putchar(x = '0');
+	x++;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
+
+
