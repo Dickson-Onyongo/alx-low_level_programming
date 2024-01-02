c8f2d37c36d3ccae0f6ad00550773d5a123e8e43
Dickson-Onyongo <dicksononyong@gmail.com>
2024-01-02 07:24:26 -0800

    alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+*Return: returns 0 upon success.
+**/
+int main(void)
+{
+	char ch(void);
+
+	for (ch = 'z'; ch >= 'a'; ch++)
+	{
+
+	putchar (ch);
+
+	}
+
+	putchar('\n');
+
+	return 0;
+
+}
