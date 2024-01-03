fbc97f71603673a5166a8ac8a8d2fbe2d7101268
Dickson-Onyongo <dicksononyong@gmail.com>
2024-01-03 09:10:06 -0800

    reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,8 +3,9 @@
 
 
 /**
-* main - Entry point.
-*
+* reset_to_98 - Takes a pointer to an int as a parameter.
+* @n: type int pointer
+* Descrption: updates the value to 98
 * Return: returns 0 upon success.
 **/
 
