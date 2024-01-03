3278739e9998bb331e6519c4227a7e0258af483a
Dickson-Onyongo <dicksononyong@gmail.com>
2024-01-03 09:03:22 -0800

    reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,28 +1,16 @@
 #include "main.h"
 #include <stdio.h>
+
+
 /**
 * main - Entry point.
 *
 * Return: returns 0 upon success.
 **/
-void reset_to_98(int *n)
 
-int main(void)
+void reset_to_98(int *n)
 {
 
-		int n = 402;
-		char ch = n;
-		int *p = &n;
-
-
-		putchar(ch);
-		putchar('\n');
-
-
-		*p = 98;
-
-		putchar((char)*p);
-
-		return (0);
+	*n = 98;
 
 }
