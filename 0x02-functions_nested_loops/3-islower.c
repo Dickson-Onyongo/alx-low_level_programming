08fecfeb35a1e5982793b50294f334a3cd2ed3f2
Dickson-Onyongo <dicksononyong@gmail.com>
2024-01-18 12:38:11 -0800

    checks if lowercase or not

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,15 +6,15 @@
  *Return: 1 if lowercase and 0 if not.
  */
 
- int _islower(int c)
- {
- 	if (c >= 'a' && c <= 'z')
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 		}
 	else
 	{
-		return(0);
+		return (0);
 	}
 
 }
