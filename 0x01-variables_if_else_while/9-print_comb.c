9edb28ec721d8c849f6c7642b7ccec0dca0f92ba
Dickson-Onyongo <dicksononyong@gmail.com>
2024-02-06 01:12:15 +0300

    program to print the possible combinations of single dogot numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdion.h>
+/**
+ * main - entry point of a program tht prints all combinations of single digit numbers.
+ * 
+ * Return: returns 0 upon success.
+ */
+
+int main ()
+{
+	int i;
+	int j;
+
+	for (int i = 0; i < 10; ++i)
+	{
+		for (int j = 0; j < 10; ++j)
+		{
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
+			putchar(j + '0');
+			putchar('\n');
+
+		}
+
+
+	}
+return (0)
+
+}
+
