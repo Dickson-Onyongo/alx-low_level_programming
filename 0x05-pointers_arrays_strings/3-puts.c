12617a2eb05b71facaf98c722d142679b0323be4
Dickson-Onyongo <dicksononyong@gmail.com>
2024-02-15 01:48:56 +0300

    function to print a string to the stdio

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * _puts - prints a string followed by new line.
+ * @*str: string pointer.
+ * @Mystr: the string
+ * Return: returns 0 upon success.
+ */
+
+void _puts(char *str)
+{
+	char Mystr[];
+	char *str = &Mstr;
+
+	_puts(str);
+
+
+	return (0);
+}
