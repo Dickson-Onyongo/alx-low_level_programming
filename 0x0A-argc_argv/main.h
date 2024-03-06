0d8b863023d070c93148cb1dd2b11fd3cb411d67
Dickson-Onyongo <dicksononyong@gmail.com>
2024-03-07 00:54:20 +0300

    main.h file

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+int main(int argc, char __attribute__((__unused__)) *argv[]);
+int main(int __attribute__((__unused__)) argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+
+#endif
