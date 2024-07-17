1722ff095de00ac91e69019fea958e3bd71d31a7
Dickson-Onyongo <dicksononyong@gmail.com>
2024-07-17 22:26:43 +0300

    linear search algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,7 +10,7 @@
  * Return: Always EXIT_SUCCESS
  */
 
-int linear_search(int *array, size_t size, int value);
+int linear_search(int *array, size_t size, int value)
 {
 	size_t x;
 
@@ -20,7 +20,7 @@ int linear_search(int *array, size_t size, int value);
 	}
 	for (x = 0; x < size; x++)
 	{
-		printf("Value checked array[%li]\n", x, array[x]);
+		printf("Value checked array[%li] = [%i]\n", x, array[x]);
 		if (array[x] == value)
 		{
 			return (x);
