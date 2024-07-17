3a17d04583bcf09811592b9acff1bda0a9b82ed4
Dickson-Onyongo <dicksononyong@gmail.com>
2024-07-17 23:35:23 +0300

    new search pdate

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,6 +1,4 @@
 #include "search_algos.h"
-
-
 /**
  * linear_skip - searches for a value conatined in a skip list; assumes a
  * list with sorted values and a single skip layer with nodes at every index
@@ -11,6 +9,7 @@
  * Return: pointer on the first node where value is located, or NULL if list
  * is NULL or value not found
  */
+
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
 	skiplist_t *temp = NULL, *stop = NULL;
